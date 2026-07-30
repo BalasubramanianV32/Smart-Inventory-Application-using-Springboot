94f71bf6a4ce432297b39033bd802f151fda1a83
Bala <vbalasubramanian559@gmail.com>
2026-07-30 19:24:29 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/inventria/InventriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.inventria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventria/InventriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
